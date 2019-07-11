f42616523208c49cf83fd332efb2f17ee15cbfb6
Brandon James <brandonj2858@gmail.com>
2019-07-11 14:14:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,4 +9,6 @@ def turn_count(board)
  end
  
  def current_player(board)
-   if turn_count % 2 == 0 
+   if turn_count(board) % 2 == 0
+     current_player(board) =+ "O"
+   else current_player(board) == "O"
